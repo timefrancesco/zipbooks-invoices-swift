2550edd846320620c6196cfc6e0b77ac6fc8e3be
Francesco Pretelli <francesco@timelabs.io>
2016-01-23 17:00:35 +1300

    removed swiftyjson reference

--- zipbooks-ios/Services/Extensions.swift
+++ zipbooks-ios/Services/Extensions.swift
@@ -10,7 +10,6 @@ import Foundation
 import UIKit
 import RealmSwift
 import ObjectMapper
-import SwiftyJSON
 
 // Based on Swift 1.2, ObjectMapper 0.15, RealmSwift 0.94.1
 // Author: Timo Wälisch <timo@waelisch.de>
