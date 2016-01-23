ada82c01749f30ce35ca2b26b0723fa496d55d4b
Francesco Pretelli <francesco@timelabs.io>
2016-01-23 16:22:10 +1300

    removed unused pods added buddybuild sdk

--- Podfile
+++ Podfile
@@ -2,12 +2,10 @@ source 'https://github.com/CocoaPods/Specs.git'
 
 platform :ios, '8.0'
 
-pod 'ReachabilitySwift', '~> 2.0'
 pod 'Alamofire', '~> 3.0'
-pod 'SwiftyJSON', '~> 2.3'
-pod 'SDWebImage', '~>3.7'
 pod 'ObjectMapper', '~> 1.0'
 pod 'AlamofireObjectMapper', '~> 2.1'
 pod 'RealmSwift', '~> 0.96'
+pod 'BuddyBuildSDK'
 
 use_frameworks!
--- zipbooks-ios/AppDelegate.swift
+++ zipbooks-ios/AppDelegate.swift
@@ -17,7 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
-        print(Utility.getToken())
+        //print(Utility.getToken())
+        BuddyBuildSDK.setup()
         
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         if Utility.getToken() == ""{
